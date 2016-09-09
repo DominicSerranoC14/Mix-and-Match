424c6cf92b1d67e4fb1563fe4392f23eb7ae9c58
Dominic Serrano <dmsrojo@gmail.com>
2016-09-08 21:08:08 -0500

    Can now build successful match message, need to resize

--- js/matchMessage.js
+++ js/matchMessage.js
@@ -3,13 +3,39 @@
 //Browserify required modules
 const $ = require('jquery');
 const { determinePair } = require('./matchCardPair');
+let { matches, misses } = require('./storage');
+
+//Variables
+const makeEl = (tag) => $(document.createElement(tag));
+const getElById = (id) => $(document.getElementById(id));
+const $outputDiv = getElById('output-div');
 /////////////////////////////////////////
 
+
+/////////////////////////////////////////
+//Build message for matches/misses
+let buildeMatchMessage = (message) => {
+
+  //Build matchMessage div
+  const $p = makeEl('p');
+  $p.text(message);
+
+  const $div = makeEl('div');
+  $div.addClass('match-message-div');
+  $div.addClass('match-div-show');
+  $div.append($p);
+  $outputDiv.append($div);
+};
+/////////////////////////////////////////
 
 
 /////////////////////////////////////////
 //Function that displays successful match message and animation
 const successfulMatch = (array) => {
+  //Add one to matches and display message string
+  matches++;
+  let message = `Correctly made ${matches}!`;
+  buildeMatchMessage(message);
 
   //Loop through each element and add success animation
   $(array).each((i, $each) => {
--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,6 @@
 'use strict';
 
+//Variables and arrays used throughout the app
 module.exports.matchArray = [];
+module.exports.matches = 0;
+module.exports.misses = 0;
