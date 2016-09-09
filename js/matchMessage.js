6bf8182d6a1a7f5875f95df38a9b1470fd9da5fb
Dominic Serrano <dmsrojo@gmail.com>
2016-09-09 09:13:59 -0500

    Made minor css changes to match message div

--- js/matchMessage.js
+++ js/matchMessage.js
@@ -14,18 +14,15 @@ const $outputDiv = getElById('output-div');
 
 /////////////////////////////////////////
 //Build message for matches/misses
-let buildeMatchMessage = (message) => {
-
-  //Build matchMessage div
-  const $p = makeEl('p');
-  $p.text(message);
-
-  const $div = makeEl('div');
-  $div.addClass('match-message-div');
-  $div.addClass('match-div-show');
-  $div.append($p);
-  $outputDiv.append($div);
-};
+// let buildMatchMessage = (message) => {
+//
+//   //Build matchMessage div
+//   const $div = makeEl('div');
+//   $div.text(message);
+//   $div.addClass('match-message-div');
+//   // $div.addClass('match-div-show');
+//   $outputDiv.append($div);
+// };
 /////////////////////////////////////////
 
 
@@ -34,8 +31,8 @@ let buildeMatchMessage = (message) => {
 const successfulMatch = (array) => {
   //Add one to matches and display message string
   matches++;
-  let message = `Correctly made ${matches}!`;
-  buildeMatchMessage(message);
+  let message = `Correctly matched ${matches}!`;
+  // buildMatchMessage(message);
 
   //Loop through each element and add success animation
   $(array).each((i, $each) => {
